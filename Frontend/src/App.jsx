2bb6d7c2233716651a41b882e2130972027d727f
Excorsite <panditsuyog4@gmail.com>
2025-05-21 13:28:45 +0545

    Add assets

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world! how you doing
-      </h1>
+    <div className="app">
+      <Navbar />
     </div>
   );
 };
