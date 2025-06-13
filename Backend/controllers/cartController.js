c0f164fb2111a9395656d6d6a9c226e9ab9bdd5d
Excorsite <panditsuyog4@gmail.com>
2025-06-13 10:48:06 +0545

    add authMiddleware, cart in the backend

---
+++ Backend/controllers/cartController.js
@@ -0,0 +1,43 @@
+import { response } from "express";
+import userModel from "../models/userModel.js";
+
+// add item to user cart
+const addToCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId] += 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Added to Cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error to add cart" });
+  }
+};
+
+// remove items from user cart
+
+const removeFormCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Removed from Cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error has occured in remove cart" });
+  }
+};
+
+// fetch user cart data
+
+const getCart = async (req, res) => {};
+
+export { addToCart, removeFormCart, getCart };
